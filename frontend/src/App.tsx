5c8006693e0e403d88987877a86c29549acbff46
Hirusha99 <hirushanethni2277@gmail.com>
2025-01-05 22:43:26 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import EmployeePage from "./pages/employeePage"
+
+
+const App = () => {
+  return (
+    <div>
+<EmployeePage/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/EmployeePage.tsx
@@ -0,0 +1,14 @@
+const EmployeePage = () => {
+  return (
+    <div>
+      <p className="h2 text-center m-5">Employee Management System</p>
+      <div className="text-end">
+        <button type="button" className="btn btn-success m-5">
+          New Employer
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeePage;
